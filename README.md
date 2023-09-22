6ea2653ba3c64a0a6a6f50dce5b927d78c3f0dad
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2023-09-22 15:21:10 +0200

    Upload up until 2022

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ### AoC questions with answers
 
 Dump of my Aoc inputs and answers.
+
+serve locally via:
+
+
+```
+python3.9 -m http.server 9999
+```
+
--- download.sh
+++ download.sh
@@ -3,24 +3,31 @@ set -eux
 
 # taken from: https://www.reddit.com/r/adventofcode/comments/rgo4m9/is_there_a_script_to_download_all_the_current/holk8cw/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 # usage:
-# ./download.sh 'session=xxx' 2022 > /dev/null 2>&1 &
+# ./download.sh 'session=xxx' > /dev/null 2>&1 &
 
 
 COOKIE=${1:-'undefined'}
-YEAR=${2:-'2015'}
 
 url="https://adventofcode.com"
-url_day="${url}/${YEAR}/day"
 
 mkdir -p static
 curl "${url}/static/style.css" > static/style.css
 
-mkdir -p ${YEAR}/day
-cd ${YEAR}/day
+curl --cookie "${COOKIE}" "${url}" > index.html
 
-for i in $(seq 25)
-do
-  mkdir -p $i
-  curl "${url_day}/${i}" > ${i}/index.html
-  curl --cookie "${COOKIE}" "${url_day}/${i}/input" > ${i}/input
+for y in {2015..2022}
+  do    
+    mkdir -p ${y}/events
+    mkdir -p ${y}/day
+    curl --cookie "${COOKIE}" "${url}/${y}" > ${y}/index.html
+    curl --cookie "${COOKIE}" "${url}/${y}/events" > ${y}/events/index.html
+    for i in $(seq 25)
+    do
+      mkdir -p ${y}/day/${i}
+      curl --cookie "${COOKIE}" "${url}/${y}/day/${i}" > ${y}/day/${i}/index.html
+      curl --cookie "${COOKIE}" "${url}/${y}/day/${i}/input" > ${y}/day/${i}/input
+      # when serving the index files, they will already have /day/1/, plus /1/input
+      mkdir -p ${y}/day/${i}/${i}
+      cp ${y}/day/${i}/input ${y}/day/${i}/${i}/input
+    done
 done
